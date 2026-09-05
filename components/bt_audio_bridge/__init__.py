8417507915fc07da9227c9555db6fb0c1205238d
MarLip1981 <julek1.mariusz@gmail.com>
2026-09-05 11:14:27 +0200

    edited

--- components/bt_audio_bridge/__init__.py
+++ components/bt_audio_bridge/__init__.py
@@ -1,1 +1,25 @@
-a
+import esphome.codegen as cg
+import esphome.config_validation as cv
+
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ["wifi"]
+
+bt_audio_bridge_ns = cg.esphome_ns.namespace("bt_audio_bridge")
+BtAudioBridge = bt_audio_bridge_ns.class_(
+    "BtAudioBridge",
+    cg.Component,
+)
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(BtAudioBridge),
+}).extend(cv.COMPONENT_SCHEMA)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+
+    # ESP32-A2DP korzysta z bibliotek Arduino.
+    # W ESPHome 2026.2+ trzeba je jawnie włączyć.
+    cg.add_library("ESP32-A2DP", None)
