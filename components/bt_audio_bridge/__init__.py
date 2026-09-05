57101353c203fd2afcfc12ebbc77fe68722edaa8
MarLip1981 <julek1.mariusz@gmail.com>
2026-09-05 15:30:44 +0200

    Integrate ESP-IDF Bluetooth Classic component
    Added include_builtin_idf_component for ESP32-A2DP support.

--- components/bt_audio_bridge/__init__.py
+++ components/bt_audio_bridge/__init__.py
@@ -2,6 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 
 from esphome.const import CONF_ID
+from esphome.components.esp32 import include_builtin_idf_component
 
 DEPENDENCIES = ["wifi"]
 
@@ -18,5 +19,9 @@ CONFIG_SCHEMA = cv.Schema({
 
 
 async def to_code(config):
+    # ESP32-A2DP korzysta bezpośrednio z ESP-IDF Bluetooth Classic.
+    # Od ESPHome 2026.2 nieużywane komponenty IDF są domyślnie wyłączane.
+    include_builtin_idf_component("bt")
+
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
