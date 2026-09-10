ae83822eaf47abbf5f1efc34e1a860454bead0e6
MarLip1981 <julek1.mariusz@gmail.com>
2026-09-10 23:00:27 +0200

    fix: correct ESPHome sensor schema key

--- components/bt_audio_bridge/__init__.py
+++ components/bt_audio_bridge/__init__.py
@@ -91,7 +91,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Optional(CONF_EVENT): text_sensor.text_sensor_schema(),
     cv.Optional(CONF_RESET_REASON): text_sensor.text_sensor_schema(),
     cv.Optional(CONF_DEVICE): text_sensor.text_sensor_schema(),
-    cv.Optional(CONF_RSSI): sensor.sensor_schema(unit_of_measureMENT="dBm", accuracy_decimals=0),
+    cv.Optional(CONF_RSSI): sensor.sensor_schema(unit_of_measurement="dBm", accuracy_decimals=0),
     cv.Optional(CONF_BATTERY): text_sensor.text_sensor_schema(),
     cv.Optional(CONF_DEVICES, default=[]): cv.All(
         cv.ensure_list(DEVICE_SLOT_SCHEMA),
