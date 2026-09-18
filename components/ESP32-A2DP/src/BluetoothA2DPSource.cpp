960c1989206afcc78e45f133637df0c47c25740e
MarLip1981 <julek1.mariusz@gmail.com>
2026-09-18 21:17:01 +0200

    Fix media gating source formatting

--- components/ESP32-A2DP/src/BluetoothA2DPSource.cpp
+++ components/ESP32-A2DP/src/BluetoothA2DPSource.cpp
@@ -1101,7 +1101,19 @@ void BluetoothA2DPSource::bt_av_hdl_avrc_ct_evt(uint16_t event, void *p_param) {
 
 void BluetoothA2DPSource::set_reset_ble(bool doInit) { reset_ble = doInit; }
 
-void BluetoothA2DPSource::set_media_enabled(bool enabled) {\n  this->media_enabled_ = enabled;\n  if (enabled) {\n    if (s_a2d_state == APP_AV_STATE_CONNECTED) {\n      esp_a2d_media_ctrl(ESP_A2D_MEDIA_CTRL_CHECK_SRC_RDY);\n    }\n  } else {\n    if (s_media_state == APP_AV_MEDIA_STATE_STARTED ||\n        s_media_state == APP_AV_MEDIA_STATE_STARTING) {\n      esp_a2d_media_ctrl(ESP_A2D_MEDIA_CTRL_SUSPEND);\n    }\n  }\n}
+void BluetoothA2DPSource::set_media_enabled(bool enabled) {
+  this->media_enabled_ = enabled;
+  if (enabled) {
+    if (s_a2d_state == APP_AV_STATE_CONNECTED) {
+      esp_a2d_media_ctrl(ESP_A2D_MEDIA_CTRL_CHECK_SRC_RDY);
+    }
+  } else {
+    if (s_media_state == APP_AV_MEDIA_STATE_STARTED ||
+        s_media_state == APP_AV_MEDIA_STATE_STARTING) {
+      esp_a2d_media_ctrl(ESP_A2D_MEDIA_CTRL_SUSPEND);
+    }
+  }
+}
 
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 0, 0)
 
